1a97811a9fce06b7927bb39a4e25f10f78f0b839
Aganazzar <christmasbhandari@gmail.com>
2025-08-05 20:52:35 +0545

    map working flow

--- app/set_preferences.jsx
+++ app/set_preferences.jsx
@@ -68,8 +68,8 @@ export default function PreferencesScreen() {
       const data = await response.json();
       if (data.success) {
         console.log("Preferences saved:", data);
-       // router.replace('/');// Redirect to the homepage after saving preferences
-        router.push('/address');
+       router.replace('/address');// Redirect to the homepage after saving preferences
+       // router.push('/address');
       }
     } catch (error) {
       console.error("Error saving preferences:", error);
