c56491283c85e90ac1d095513140a626d5aa453a
Aganazzar <christmasbhandari@gmail.com>
2025-08-05 19:22:27 +0545

    working rn

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 // config.js
-const LOCAL_IP = "192.168.1.67"; // ⚠️ Replace this with your Mac's local IP address
-export const BASE_URL = `http://localhost:8000`;
+const LOCAL_IP = "192.168.1.95"; // ⚠️ Replace this with your Mac's local IP address
+export const BASE_URL = `http://${LOCAL_IP}:8000`;
